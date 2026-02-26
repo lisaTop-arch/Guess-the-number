9146c669489012cecf2d7af92251d470546d6d5f
lisaTop-arch <alisa.krass009@gmail.com>
2026-02-26 09:51:22 -0500

    Create number_guessing_game.py
    Initial version of number guessing game. WROTE WITH CHATGPT

---
+++ number_guessing_game.py
@@ -0,0 +1,13 @@
+import random
+
+number_to_guess = random.randint(1, 10)
+guess = None
+
+while guess != number_to_guess:
+    guess = int(input("Guess a number between 1 and 10: "))
+    if guess < number_to_guess:
+        print("Too low!")
+    elif guess > number_to_guess:
+        print("Too high!")
+    else:
+        print("You got it!")
